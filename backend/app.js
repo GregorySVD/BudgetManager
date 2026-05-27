3ea96c2a1b0a26a9edbc8854c5e6c95ed6d7ccff
GTerenda <grzegorzterenda@gmail.com>
2026-05-27 20:30:43 +0200

    Enable CORS for frontend requests

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 require("./config/database");
 
 const transactionRoutes = require("./routes/transactionRoutes");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/transactions", transactionRoutes);
